b5cfd770762191bd47b4cc10328735dc17bc1c15
Will Fantom <w.fantom@lancaster.ac.uk>
2018-11-10 11:04:14 +0000

    Added Database

---
+++ schema.sql
@@ -0,0 +1,19 @@
+-- Initialize the database.
+-- Drop any existing data and create empty tables.
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE list (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  uid INTEGER NOT NULL,
+  added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  uri TEXT NOT NULL,
+  FOREIGN KEY (uid) REFERENCES user (id)
+);
